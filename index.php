4c3ee5740071255023922c34540bd0aa0806e63d
root <root@localhost.localdomain>
2016-08-17 18:49:05 +0530

    work

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ if(version_compare(PHP_VERSION, '5.3.7','<')) {
 	exit("Sorry, this code doesn't run on a PHP version smaller than 5.3.7!");
 }
 
+else {
+	header('Location: index.html');
+}
 
-require_once("page.php");
-$page = new Page();
+
 
 ?>
